169049b79a6a13430b81e11d0235d0d30fe100ce
August Lilleaas <august@augustl.com>
2011-09-28 21:08:11 +0200

    Adding groups with a resource set.

---
+++ test/group-test.js
@@ -0,0 +1,26 @@
+var buster = require("buster");
+var assert = buster.assert;
+var refute = buster.refute;
+var bcGroup = require("../lib/group");
+
+buster.testCase("buster-configuration group", {
+    "should create resources with root path": function (done) {
+        var group = bcGroup.create({resources: ["foo.js", "bar.js"]}, __dirname + "/fixtures");
+        group.resolve().then(function () {
+            assert("/foo.js" in group.resourceSet.resources);
+            assert("/bar.js" in group.resourceSet.resources);
+            done();
+        });
+    },
+
+    "should get file contents as actual content": function (done) {
+        var group = bcGroup.create({resources: ["foo.js"]}, __dirname + "/fixtures");
+        group.resolve().then(function () {
+            group.resourceSet.getResource("/foo.js", function (err, resource) {
+                assert.isUndefined(err);
+                assert.equals(resource.content, "var thisIsTheFoo = 5;");
+                done();
+            });
+        });
+    },
+});
