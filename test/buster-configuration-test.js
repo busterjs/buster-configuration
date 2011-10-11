2427a4cf4d88a7a06ba8764d069af6ad65e0806c
August Lilleaas <august@augustl.com>
2011-10-11 21:24:46 +0200

    Removing everyting, preparing for rewrite.

--- test/buster-configuration-test.js
+++
@@ -1,8 +0,0 @@
-var buster = require("buster");
-var assert = buster.assert;
-var busterConfiguration = require("../lib/buster-configuration");
-
-buster.testCase("buster-configuration", {
-    setUp: function () {
-    }
-});
