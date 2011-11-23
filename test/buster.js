90533e3c063a1664df7de0ff2ca815b33a18f7ea
August Lilleaas <august@augustl.com>
2011-11-23 21:44:10 +0100

    Updating config file for latest APIs.

--- test/buster.js
+++ test/buster.js
@@ -3,7 +3,7 @@ var config = module.exports;
 config["Tests"] = {
     environment: "node",
     load: [
-        "lib/*.js",
-        "test/*-test.js"
+        "../lib/*.js",
+        "*-test.js"
     ]
 };
